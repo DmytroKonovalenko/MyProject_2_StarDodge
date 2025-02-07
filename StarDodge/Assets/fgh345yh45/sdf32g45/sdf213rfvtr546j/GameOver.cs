a4de17cbde62152f54967bd3b59f60507954f07f
DmytroKonovalenko <112824376+DmytroKonovalenko@users.noreply.github.com>
2025-02-07 20:19:15 +0200

    obfuscated
    obfuscated

---
+++ StarDodge/Assets/fgh345yh45/sdf32g45/sdf213rfvtr546j/GameOver.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+    public int continuePrice;
+
+    public RocketController rocketController;
+    public GameObject menu;
+    public TextMeshProUGUI priceText;
+    public Animation notEnough;
+    public LevelLoader levelLoader;
+    [HideInInspector]
+    public bool crashed;
+
+    public static GameOver instance;
+
+    private Animation anim;
+
+    void Start()
+    {
+        instance = this;
+        anim = this.GetComponent<Animation>();
+        priceText.text = continuePrice.ToString();
+        crashed = false;
+    }
+    public void Crashed()
+    {
+       
+        crashed = true;
+        rocketController.Crashed();
+        anim.Play("Game-Over-In");
+        menu.SetActive(false);
+    }
+
+    public void Continue()
+    {
+        if(Wallet.GetAmount() >= continuePrice)
+        {
+            Wallet.SetAmount(Wallet.GetAmount() - continuePrice);
+            Score_SAD.continueGame = true;
+            levelLoader.LoadLevel(1);
+        }
+        else
+        {
+            notEnough.Play("Not-Enough-In");
+        }
+    }
+}
